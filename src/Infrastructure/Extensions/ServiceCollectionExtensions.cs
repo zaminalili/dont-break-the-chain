b7a36d2cdd88f89d27ddc67961f76d45cdd7f975
zaminalili <zaminalili29@gmail.com>
2025-05-04 17:20:26 +0400

    Establish connection

--- src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
+++ src/Infrastructure/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Extensions
@@ -8,6 +10,11 @@ namespace Infrastructure.Extensions
         public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
         {
 
+            // Configure database context
+            services.AddDbContext<DBChDbContext>(opt =>
+                opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+            
+            // Other services like repositories, caching, etc.
         }
     }
 }
