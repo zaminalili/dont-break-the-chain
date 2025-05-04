27f17e894ab29561e8e8c738230302a370ad4600
zaminalili <zaminalili29@gmail.com>
2025-05-04 17:15:49 +0400

    Fix relation in UserSettings

--- src/Domain/Entities/UserSettings.cs
+++ src/Domain/Entities/UserSettings.cs
@@ -5,7 +5,6 @@ public class UserSettings
     public Guid Id { get; set; } = Guid.NewGuid();
 
     public Guid UserId { get; set; }
-    public User User { get; set; } = default!;
 
     //public bool EnableNotifications { get; set; }
     public string? PreferredLanguage { get; set; }
