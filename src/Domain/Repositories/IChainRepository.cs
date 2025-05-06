02ee27b357a0c329ef633bb82bd059c26aa88f0b
zaminalili <zaminalili29@gmail.com>
2025-05-06 10:38:56 +0400

    Add IChainRepository

---
+++ src/Domain/Repositories/IChainRepository.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+
+namespace Domain.Repositories;
+
+public interface IChainRepository : IRepositoryBase<Chain>
+{
+    Task<(IEnumerable<Chain>, int)> GetPublicChainsByUserAsync(Guid userId, int pageNumber = 1, int pageSize = 3);
+    Task<(IEnumerable<Chain>, int)> GetChainsByUserAsync(Guid userId, bool isPublic = true, int pageNumber = 1, int pageSize = 3);
+    Task<(IEnumerable<Chain>, int)> GetChainsByUserAndCategoryAsync(Guid userId, Guid categoryId, bool isPublic = true, int pageNumber = 1, int pageSize = 3);
+    Task<(IEnumerable<Chain>, int)> GetPublicChainsByCategoryAsync(Guid categoryId, int pageNumber = 1, int pageSize = 15);
+    Task<(IEnumerable<Chain>, int)> GetPublicChainsByChallengeAsync(Guid challengeId, Guid categoryId, int pageNumber = 1, int pageSize = 15);
+
+    Task<int> GetChainCountAsync();
+    Task<int> GetChainCountAsync(Guid categoryId);
+}
