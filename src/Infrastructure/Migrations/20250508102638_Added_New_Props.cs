cc95b468bf84c558fe48ec7c1eac3bee68180d52
zaminalili <zaminalili29@gmail.com>
2025-05-08 14:29:15 +0400

    Add props to entities

---
+++ src/Infrastructure/Migrations/20250508102638_Added_New_Props.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_New_Props : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsBroken",
+                table: "Chains",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "LongStreak",
+                table: "Chains",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "ChainEntries",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsBroken",
+                table: "Chains");
+
+            migrationBuilder.DropColumn(
+                name: "LongStreak",
+                table: "Chains");
+
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "ChainEntries");
+        }
+    }
+}
