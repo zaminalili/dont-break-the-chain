20ff65ef6958621cd6a7a3e29fa642b7c08740fb
zaminalili <zaminalili29@gmail.com>
2025-05-09 14:02:09 +0400

    Add data annotations to dtos for validation

--- src/Application/DTOs/UpdateChainDto.cs
+++ src/Application/DTOs/UpdateChainDto.cs
@@ -1,10 +1,21 @@
-﻿namespace Application.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Application.DTOs;
 
 public class UpdateChainDto
 {
+    [Required]
     public Guid Id { get; set; }
+    
+    [Required]
+    [MaxLength(20)]
+    [MinLength(3)]
     public string Title { get; set; } = default!;
+
+    [MaxLength(250)]
     public string? Description { get; set; }
     public bool IsPublic { get; set; }
+
+    [Required]
     public Guid CategoryId { get; set; }
 }
