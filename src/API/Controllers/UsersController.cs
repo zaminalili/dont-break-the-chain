fcb97c8244ec98b4abf1ad92d0cb4a30b55d1dae
zaminalili <zaminalili29@gmail.com>
2025-05-09 14:32:42 +0400

    Add user endpoints for chain

---
+++ src/API/Controllers/UsersController.cs
@@ -0,0 +1,59 @@
+﻿using API.Attributes;
+using Application.DTOs;
+using Application.Services.Abstract;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("v1/[controller]/{userId:guid}")]
+    [ApiController]
+    [Authorize]
+    [ValidateModel]
+    public class UsersController(IChainService chainService) : ControllerBase
+    {
+        [HttpPost]
+        [Route("chains")]
+        public async Task<IActionResult> Create([FromRoute] Guid userId, [FromBody] CreateChainDto request)
+        {
+            request.UserId = userId;
+            await chainService.CreateChainAsync(request);
+
+            return Ok("Chain created successfully");
+        }
+
+        [HttpGet]
+        [Route("chains")]
+        public async Task<ActionResult<ResponseDto<IEnumerable<ChainDto>>>> GetChainsByUserId([FromRoute] Guid userId, [FromQuery] ChainsRequestDto request)
+        {
+            request.Id = userId;
+            var response = await chainService.GetChainsByUserIdAsync(request);
+            return Ok(response);
+        }
+
+        [HttpGet]
+        [Route("chains/{chainId}")]
+        public async Task<ActionResult<ResponseDto<ChainDto>>> GetChainById([FromRoute] Guid userId, [FromRoute] Guid chainId)
+        {
+            var response = await chainService.GetChainByIdAsync(chainId);
+            return Ok(response);
+        }
+
+        [HttpDelete]
+        [Route("chains/{chainId}")]
+        public async Task<IActionResult> DeleteChain([FromRoute] Guid userId, [FromRoute] Guid chainId)
+        {
+            await chainService.DeleteChainAsync(chainId);
+            return Ok("Chain deleted successfully");
+        }
+
+        [HttpPut]
+        [Route("chains/{chainId}")]
+        public async Task<ActionResult<ChainDto>> UpdateChain([FromRoute] Guid userId, [FromRoute] Guid chainId, [FromBody] UpdateChainDto request)
+        {
+            request.Id = chainId;
+            var response = await chainService.UpdateChainAsync(request);
+            return Ok(response);
+        }
+    }
+}
