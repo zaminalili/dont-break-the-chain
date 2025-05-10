aecb00c3b7fd9919f3daef73d341bacc181d7dff
zaminalili <zaminalili29@gmail.com>
2025-05-10 22:19:40 +0400

    Add DTOs for ChainEntry

---
+++ src/Application/DTOs/ChainEntryDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Application.DTOs;
+
+public class ChainEntryDto
+{
+    public Guid Id { get; set; }
+    public DateTime Date { get; set; }
+    public string? Note { get; set; }
+    public string? ImageUrl { get; set; }
+    public bool IsCompleted { get; set; }
+    public Guid ChainId { get; set; }
+}
