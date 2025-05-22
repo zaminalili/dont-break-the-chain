3a90813fe97f58d008f885a86172a19696b96293
zaminalili <zaminalili29@gmail.com>
2025-05-22 12:41:45 +0400

    Add IImageValidator

---
+++ src/Infrastructure/Services/IImageValidator.cs
@@ -0,0 +1,9 @@
+﻿using Infrastructure.Models;
+using Microsoft.AspNetCore.Http;
+
+namespace Infrastructure.Services;
+
+public interface IImageValidator
+{
+    Task<bool> IsMatchAsync(IFormFile image, string description);
+}
