5a72ba3415e8e49ccf4b00d262f207d2cc3d973e
zaminalili <zaminalili29@gmail.com>
2025-05-26 16:34:00 +0400

    Add IsDeactive prop to Category

--- src/Domain/Entities/Category.cs
+++ src/Domain/Entities/Category.cs
@@ -5,6 +5,7 @@ public class Category
     public Guid Id { get; set; } = Guid.NewGuid();
     public string Name { get; set; } = default!;
     public string? Icon { get; set; }
+    public bool IsDeactive { get; set; }
 
 
     // Navigation properties
---
+++ src/Infrastructure/Migrations/20250526123036_Added_IsDeactive_prop_to_Category.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Added_IsDeactive_prop_to_Category : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeactive",
+                table: "Categories",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsDeactive",
+                table: "Categories");
+        }
+    }
+}
