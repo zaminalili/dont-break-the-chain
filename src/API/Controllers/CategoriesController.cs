266bcb44450f3186f55fa2169c679033afa283b6
zaminalili <zaminalili29@gmail.com>
2025-05-26 17:16:28 +0400

    Add GetAllActive endpoint for Category

---
+++ src/API/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTOs;
+using Application.Services.Abstract;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[Route("v1/[controller]")]
+[ApiController]
+public class CategoriesController(ICategoryService categoryService) : ControllerBase
+{
+    [HttpGet]
+    [ProducesResponseType(typeof(CategoryDto), StatusCodes.Status200OK, "application/json")]
+    public async Task<ActionResult<CategoryDto>> GetAllActive()
+    {
+        var categories = await categoryService.GetAllAsync();
+
+        return Ok(categories);
+    }
+}
